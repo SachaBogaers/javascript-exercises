93916971f26329e01f61b3965b1b1917a57a3ef6
Sacha <48031121+SachaBogaers@users.noreply.github.com>
2021-02-27 16:22:43 +0100

    Add files via upload

---
+++ javascript_opdracht1/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc Academy");
+let firstName = "Sacha";
+console.log(firstName);
+console.log(4 + 4);
+console.log("4" + "4");
+console.log(5 + 10);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 3);
+let age = "25";
+console.log(typeof (age))
