36c43e568dcb2584410ee07d58c0968395467935
Sacha <48031121+SachaBogaers@users.noreply.github.com>
2021-02-28 13:40:29 +0100

    Add files via upload

---
+++ javascript_opdracht6/index.js
@@ -0,0 +1,19 @@
+const makePizza = function () {
+	console.log("Make dough");
+	console.log("Put ingredients on dough");
+	console.log("Put pizza in the oven");
+}
+makePizza();
+
+
+const makeSushi = function () {
+	console.log("Boil rice");
+	console.log("Cut ingredients");
+	console.log("Roll sushi");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
