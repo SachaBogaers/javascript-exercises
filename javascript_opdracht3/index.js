d59ddc55b0477c041086612e749f8c6d36ba5dd0
Sacha <48031121+SachaBogaers@users.noreply.github.com>
2021-02-28 12:33:53 +0100

    Add files via upload

---
+++ javascript_opdracht3/index.js
@@ -0,0 +1,22 @@
+const age = 17;
+
+if (age >= 18) {
+	console.log("Woohoo, you are old enough to enter!")
+} else if (age < 18) {
+	console.log("Sorry, you are not allowed to enter.")
+}
+
+
+const isFemale = false;
+if (isFemale) {
+	console.log("Woohoo, you can join our Ladies Night!")
+} else {
+	console.log("Sorry, you are not allowed to enter.")
+}
+
+const driverStatus = "bob";
+if (driverStatus == "bob") {
+	console.log("Woohoo,  you are allowed to drive!")
+} else {
+	console.log("Sorry, you are not allowed to drive.")
+}
